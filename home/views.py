96d79f82e3165207a5516acb3e171015f5f781de
khrist14n <khrist14n@gmail.com>
2020-06-29 07:35:47 +0000

    links

--- home/views.py
+++ home/views.py
@@ -3,15 +3,23 @@ from django.http import HttpResponse
 
 def index(request):
     message = '<h1>App de Django</h1>'
+    message = message + '<p><a href="/home">Inicio</a></p>'
+    message = message + '<p><a href="/link">Enlace</a></p>'
     return HttpResponse(message)
 
 
 def home(request):
     message = '<h1>Home de Django</h1>'
     message = message + "<p>Informacion de Django</p>"
+    message = message + '<p><a href="/home">Inicio</a></p>'
+    message = message + '<p><a href="/link">Enlace</a></p>'
     return HttpResponse(message)
 
 def link(request):
     message = '<h1>Enlace de Django</h1>'
     message = message + "<p>Enlace de Django</p>"
+    message = message + '<p><a href="/home">Inicio</a></p>'
+    message = message + '<p><a href="/link">Enlace</a></p>'
     return HttpResponse(message)    
+
+    
