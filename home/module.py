0045d1ea983b8cdb76e142866afe98c010cc293b
vagrant <vagrant@ubuntu1804.localdomain>
2020-06-28 23:57:37 -0700

    first commit

---
+++ home/module.py
@@ -0,0 +1,5 @@
+
+module                  = 'home'
+module_settings         = module + '.settings'
+module_urls             = module + ".urls"
+module_application      = module + '.wsgi.application'
